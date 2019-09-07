14508fa65d7af30654c0c4ae4a2adcba658416a1
Ndundiro_Kamau <ndundirokamau@gmail.com>
2019-09-08 00:37:08 +0300

    Adds a title in the index.html  and renders successfully.

--- app/__init__.py
+++ app/__init__.py
@@ -1,8 +1,13 @@
 from flask import Flask
+from .config import DevConfig
 
 
 # initialising application
 app = Flask(__name__)
+
+
+# setting up configurations
+app.config.from_object(DevConfig)
 
 from app import views
 
--- app/views.py
+++ app/views.py
@@ -8,4 +8,5 @@ def index():
     '''
     View root page function that returns the index page and its data
     '''
-    return render_template('index.html')
+    title = "News App - Where news live on"
+    return render_template('index.html' , title = title)
